54d4d7116f9569c45c9fd279ed4f1581aef372d2
Eyal Kapiluto <eyalkapi@gmail.com>
2026-08-06 11:19:47 +0300

    Add dir-relative search keymaps to telescope

--- init.lua
+++ init.lua
@@ -425,6 +425,15 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sif', function()
+        local dir = vim.fn.expand '%:p:h'
+        print(dir)
+        require('telescope.builtin').find_files { cwd = dir }
+      end, { desc = '[S]earch [I]n active dir [F]iles' })
+      vim.keymap.set('n', '<leader>sig', function()
+        local dir = vim.fn.expand '%:p:h'
+        require('telescope.builtin').live_grep { cwd = dir }
+      end, { desc = '[S]earch [I]n active dir live [G]rep' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
